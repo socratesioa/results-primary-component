4fea1dea83e94721d3d1ed6da7882e5172544f1b
socratesioa <156105096+socratesioa@users.noreply.github.com>
2025-04-17 17:20:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+let scores = [];
+
+fetch("/data.json")
+  .then((response) => {
+    if (!response.ok) return console.log("Oops! Something went wrong.");
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    scores = data;
+    renderCards("score-summary");
+  });
+
+function renderCards() {
+  const scoreCard = document.getElementById("scores-container");
+  const averageDiv = document.getElementById("average");
+  scoreCard.innerHTML = "";
+
+  let totalScore = 0;
+
+  scores.forEach((item) => {
+    const { category, score, icon } = item;
+    const categoryClass = category.toLowerCase().replace(/\s+/g, "-");
+    const row = document.createElement("div");
+    row.className = `result-row ${categoryClass}`;
+
+    row.innerHTML = `
+    <div class="score-category">
+    <img src="${icon}"/>
+    <p>${category}</p>
+    </div>
+    <p class="score-text">${score}<span> / 100</span></p>
+    `;
+
+    scoreCard.appendChild(row);
+
+    totalScore += score / scores.length;
+  });
+
+  const averageScore = totalScore;
+  averageDiv.innerHTML = `${Math.round(averageScore)}`;
+}
